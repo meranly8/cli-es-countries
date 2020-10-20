8f295e1b94ad08b173ae074ce20b050b2e0a8669
Michelle Ranly <meranly8@gmail.com>
2020-10-20 00:11:24 +0000

    Details added to selected country's display

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,10 +4,10 @@ class CLI
     API.fetch_countries
 
     sleep(1)
-    self.view
+    self.open_menu
   end
 
-  def view
+  def open_menu
     puts "Would you like to learn more about Spanish speaking countries?"
     puts "Enter 'y' to view the country directory or any other key to exit"
 
@@ -17,7 +17,7 @@ class CLI
       puts "¡Muy bien! Here's a directory of the countries that speak Español."
       puts "\n"
 
-      sleep(2)
+      sleep(3)
       display_list_of_countries
       ask_for_country_selection
       #ask for input of which country to view details of
@@ -38,19 +38,28 @@ class CLI
     country_index = gets.strip.to_i-1
 
     until country_index.between?(0, Country.all.length-1)
-      puts "¡No bueno! Please try another selection."
+      puts "¡No bueno! Invalid selection. Try another selection, por favor."
       puts "\n"
       sleep(1)
       country_index = gets.strip.to_i-1
     end
     #save selection as index of country in Country array
-    country_selection = Country.all.[country_index]
+    country_selection = Country.all[country_index]
     #use selection to display that country's details
+    display_selected_countrys_details(country_selection)
+  end
 
-    def display_selected_countrys_details(country)
-      sleep(1)
-      
-    end
+  def display_selected_countrys_details(country)
+    sleep(1)
+    puts "\n"
+    puts "       #{country.name.upcase}"
+    puts "   Name:    #{country.name}"
+    puts "   Capital: #{country.capital}"
+    puts "   Region:  #{country.region}"
+    puts "            #{country.subregion}"
+    puts "\n"
+
+    
   end
 
 end
