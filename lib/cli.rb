e82e847d78dad7861de187129ab0ad1d83dced24
Michelle Ranly <meranly8@gmail.com>
2020-10-21 05:01:12 +0000

    Removed comments & reformatted displays

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,73 +3,71 @@ class CLI
 
   def start
     puts "¡Hola! ¿Cómo estás?"
-    API.fetch_countries #grabs data from API and creates instances
+    API.fetch_countries
 
     sleep(1)
-    @counter = 0 #initialize counter for message logic
-    self.open_menu #begin program logic
+    @counter = 0
+    self.open_menu
   end
 
   def open_menu
-    if self.counter == 0 #If this is the first run of the progam
+    if self.counter == 0
       puts "\n"
-      puts "Would you like to learn more about Spanish speaking countries?" #prompt the user
-      sleep(1)
-      puts "Enter 'y' to view the country directory or any other key to exit" #instructions for input
-      self.counter += 1 #increase counter
-    else #If this is an additional run of the program
-      sleep(3)
+      puts "Would you like to learn about Spanish speaking countries?"
+      puts "Enter 'y' to view the country directory or any other key to exit."
+      self.counter += 1
+    else
+      sleep(4)
       puts "\n"
-      puts "Would you like to learn more about another?" #prompt the user
-      sleep(1)
-      puts "Enter 'y' to view the country directory or any other key to exit" #instructions for input
+      puts "Would you like to learn more about another?"
+      puts "Enter 'y' to view the country directory or any other key to exit."
     end
 
-    user_input = gets.strip.downcase #waits for & takes user input, remove whitespace & standardize input to be lowercase
+    user_input = gets.strip.downcase
 
-    if user_input == "y" #If user enters y
+    if user_input == "y"
       puts "¡Muy bien! Here's a directory of the countries that speak Español."
       puts "\n"
 
       sleep(3)
-      display_list_of_countries #call the method to display the list of Countries to choose from
-      ask_for_country_selection #call method to ask user which Country to see
+      display_list_of_countries
+      ask_for_country_selection
 
-      open_menu #call the method to run it again from the top
-    else #if user declines with other key
-      puts "¡OK, adios!" #say goodbye to user and exit
+      open_menu
+    else
+      puts "¡OK, adios!"
     end
   end
 
   def display_list_of_countries
-    puts "Country Directory".upcase #title of directory
-    Country.all.each.with_index(1) do |country, index| #iterate through array of all Country instances and their index starting at 1
-      puts "#{index}. #{country.name}" #to display each list number and Country name
+    puts "           Country Directory".upcase
+    Country.all.each.with_index(1) do |country, index|
+      puts "   #{index}. #{country.name}"
     end
   end
 
   def ask_for_country_selection
     puts "\n"
-    puts "Please enter the number of the country to learn more about it." #prompt user
-    puts "Select a number 1-#{Country.all.length}." #instructions for input
+    puts "Please enter the number of the country to learn more about it."
+    puts "Select a number 1-#{Country.all.length}."
 
-    country_index = gets.strip.to_i-1 #waits for & takes user input, remove whitespace, convert to integer & subtract 1 to align with Country instances array index
-    #error handling & messages
-    until country_index.between?(0, Country.all.length-1) #until the input of the country_index is between 0 & Country instances array length -1 because array index starts at 0
-      puts "¡No bueno! Invalid selection. Try another, por favor." #error message to prompt user of other input
+    country_index = gets.strip.to_i-1
+
+    until country_index.between?(0, Country.all.length-1)
+      puts "¡No bueno! Invalid selection. Try another, por favor."
       sleep(1)
-      country_index = gets.strip.to_i-1 #ask for user input again to select country's index
+      country_index = gets.strip.to_i-1
     end
 
-    country_selection = Country.all[country_index] #save selection as index of country in Country array
+    country_selection = Country.all[country_index]
 
-    display_selected_countrys_details(country_selection) #use selection to display that country's details
+    display_selected_countrys_details(country_selection)
   end
 
-  def display_selected_countrys_details(country) #accept argument of country to display based on index chosen
+  def display_selected_countrys_details(country)
     puts "¡Excelente!"
     sleep(1)
-    puts "\n" #display the details of the country
+    puts "\n"
     puts "          #{country.name.upcase}"
     puts "   Native Name: #{country.native_name}"
     puts "   Capital:     #{country.capital}"
@@ -78,10 +76,10 @@ class CLI
     puts "\n"
     puts "   Demonym:     #{country.demonym}"
     puts "   Population:  #{country.population}"
-    if country.borders.length == 0 #check to see if country is an island or not
+    if country.borders.length == 0
       puts "   Borders:     #{country.name} is an island"
     else
-      puts "   Borders:     #{country.borders.join(", ")}" #join borders array to display as string with elements separated by commas
+      puts "   Borders:     #{country.borders.join(", ")}"
     end
     puts "   Currency:    (#{country.currency_symbol}) #{country.currency_name}"
   end
